fe438f7909757e2cc0763160b01b7aeaf46749d2
DiogoF9 <diogof@ua.pt>
2020-02-28 13:49:23 +0000

    creating variable status

--- atlascar2-pkg/src/ReceiveMessage.cpp
+++ atlascar2-pkg/src/ReceiveMessage.cpp
@@ -22,20 +22,34 @@
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
+struct rawdata{
+            int velocity;
+            int direction;
+}
+
+struct nominalvalues{
+            int velocity;
+            int direction;
+}
+
+struct Status{
+            struct can_frame frame;
+            int nominal_value;
+            }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "ReceiveMessage");
-    ros::NodeHandle n;
+        ros::init(argc, argv, "ReceiveMessage");
+        ros::NodeHandle n;
 
-    int s;
-    int nbytes_rcv;
-	struct sockaddr_can addr;
-	struct can_frame frame;
-	struct ifreq ifr;
+        int s;
+        int nbytes_rcv;
+        struct sockaddr_can addr;
+        struct can_frame frame;
+        struct ifreq ifr;
 
-    /*name of the can device - vcan0 in case of virtual CAN bus*/
-    const char *ifname = "vcan0";
+        /*name of the can device - vcan0 in case of virtual CAN bus*/
+        const char *ifname = "vcan0";
 
 	if((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
 		perror("Error while opening socket");
@@ -53,33 +67,55 @@ int main(int argc, char **argv)
 		return -2;
 	}
 
-    /* read CAN frames */
-	while (1)
+        ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+        ros::Rate loop_rate(50000);
+  
+        int count = 0;
+        int velocidade, angulo;
+        while (ros::ok())
 	{	
 	        nbytes_rcv = recv(s, &frame, sizeof(struct can_frame),0);
 
-            if (nbytes_rcv < 0) {
-                    perror("can raw socket read");
-                    return 1;
-            }
-            if (nbytes_rcv < sizeof(struct can_frame)) {
-                    fprintf(stderr, "read: incomplete CAN frame\n");
-                    return 1;
-            }
-            
-            //printf("message received: %03X  %d  %02X %02X %02X %02X %02X %02X %02X %02X\n",frame.can_id, frame.can_dlc, frame.data[0], frame.data[1], frame.data[2], frame.data[3], frame.data[4], frame.data[5], frame.data[6], frame.data[7]);    }
-            // velocidade
-            if (frame.can_id == 0x412)
-            {
-                    //printf("velocidade: %d    ",frame.data[1]);
-                    int velocidade = frame.data[1];
-            }
-            // direcao
-            if (frame.can_id == 0x236)
-            {
-                    printf("angulo: %d\n",(frame.data[0]*256+frame.data[1]-4096)/2);
-                    int angulo = (frame.data[0]*256+frame.data[1]-4096)/2;
-            }
+                if (nbytes_rcv < 0) {
+                        perror("can raw socket read");
+                        return 1;
+                }
+                if (nbytes_rcv < sizeof(struct can_frame)) {
+                        fprintf(stderr, "read: incomplete CAN frame\n");
+                        return 1;
+                }
 
-    }
+                
+                //printf("message received: %03X  %d  %02X %02X %02X %02X %02X %02X %02X %02X\n",frame.can_id, frame.can_dlc, frame.data[0], frame.data[1], frame.data[2], frame.data[3], frame.data[4], frame.data[5], frame.data[6], frame.data[7]);    }
+                // velocidade
+                if (frame.can_id == 0x412)
+                {
+                        //printf("velocidade: %d    ",frame.data[1]);
+                        velocidade = frame.data[1];
+                }
+                // direcao
+                if (frame.can_id == 0x236)
+                {
+                        // printf("angulo: %d\n",(frame.data[0]*256+frame.data[1]-4096)/2);
+                        angulo = (frame.data[0]*256+frame.data[1]-4096)/2;
+                }
+
+                 std_msgs::String msg;
+
+                std::stringstream ss;
+                ss << "velocidade: " << velocidade;
+                ss << "         angulo: " << angulo;
+                msg.data = ss.str();
+
+                ROS_INFO("%s", msg.data.c_str());
+
+                chatter_pub.publish(msg);
+
+                ros::spinOnce();
+                
+                loop_rate.sleep();
+                ++count;
+
+        }
+        return 0;
 }
